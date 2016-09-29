0a6f1a43e507226836e74f7bdc8f5d4ccc4d1c9c
Maryan Mayher <mr.maykher@gmail.com>
2016-09-29 14:02:43 +0300

    deleted

--- EPAM Practice Task 1/Program.cs
+++ EPAM Practice Task 1/Program.cs
@@ -15,8 +15,6 @@ namespace EPAM_Practice_Task_1
             {
                 Console.WriteLine("Vect c = {0}", v); ;
             }
-
-            Console.WriteLine("Some string");
         }
     }
 }
